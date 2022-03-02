5e8f50245bec6b5763cb3744ba47344a49e93350
Prashik <prashikkamble.pnk54@gmail.com>
2022-03-02 16:56:29 +0530

    commit

---
+++ Palindrome number.py
@@ -0,0 +1,20 @@
+print('Enter a number: ')
+num = int(input())
+absnum = abs(num)
+
+rev = absnum % 10
+absnum = absnum // 10
+
+while(absnum > 0):
+    r = absnum % 10
+    absnum = absnum // 10
+    rev = rev*10 + r
+
+if(num < 0):
+    rev = rev - 2*rev
+
+if(rev == num):
+    print('Palindrome')
+
+else:
+    print('Not palindrome')
